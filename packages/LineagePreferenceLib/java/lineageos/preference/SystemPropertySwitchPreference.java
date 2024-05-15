5cf030563e23858ce63980f0a34ac57a698d1a4d
someone5678 <someone5678@users.noreply.github.com>
2024-05-15 22:43:58 +0900

    SystemPropertySwitchPreference: Adapt to LineageOS style
    * Based on SelfRemovingSwitchPreference
    
    Change-Id: I4fe2bec3a7eb085cf51ec12884b9bde0d08d9bd7

--- packages/LineagePreferenceLib/java/lineageos/preference/SystemPropertySwitchPreference.java
+++ packages/LineagePreferenceLib/java/lineageos/preference/SystemPropertySwitchPreference.java
@@ -19,25 +19,53 @@
 package lineageos.preference;
 
 import android.content.Context;
+import android.os.SystemProperties;
 import android.util.AttributeSet;
 
-import androidx.preference.SwitchPreference;
+import androidx.preference.PreferenceDataStore;
+import androidx.preference.PreferenceViewHolder;
+import androidx.preference.SwitchPreferenceCompat;
 
-public class SystemPropertySwitchPreference extends SwitchPreference {
+public class SystemPropertySwitchPreference extends SwitchPreferenceCompat {
+
+    private final ConstraintsHelper mConstraints;
 
     public SystemPropertySwitchPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        setPreferenceDataStore(new SystemPropertiesStore());
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
     }
 
     public SystemPropertySwitchPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setPreferenceDataStore(new SystemPropertiesStore());
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
     }
 
     public SystemPropertySwitchPreference(Context context) {
         super(context);
-        setPreferenceDataStore(new SystemPropertiesStore());
+        mConstraints = new ConstraintsHelper(context, null, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
     }
 
     @Override
@@ -49,5 +77,47 @@ public class SystemPropertySwitchPreference extends SwitchPreference {
         // Instead, we better do
         setChecked(restoreValue ? getPersistedBoolean((Boolean) defaultValue)
                 : (Boolean) defaultValue);
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public boolean getBoolean(String key, boolean defValue) {
+            return SystemProperties.getBoolean(key, defValue);
+        }
+    
+        @Override
+        public int getInt(String key, int defValue) {
+            return SystemProperties.getInt(key, defValue);
+        }
+
+        @Override
+        public long getLong(String key, long defValue) {
+            return SystemProperties.getLong(key, defValue);
+        }
+
+        @Override
+        public String getString(String key, String defValue) {
+            return SystemProperties.get(key, defValue);
+        }
+
+        @Override
+        public void putBoolean(String key, boolean value) {
+            putString(key, value ? "1" : "0");
+        }
+
+        @Override
+        public void putInt(String key, int value) {
+            putString(key, Integer.toString(value));
+        }
+
+        @Override
+        public void putLong(String key, long value) {
+            putString(key, Long.toString(value));
+        }
+
+        @Override
+        public void putString(String key, String value) {
+            SystemProperties.set(key, value);
+        }
     }
 }
